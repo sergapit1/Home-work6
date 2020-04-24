a60d98bc8b9a9d91ce17514f745fa5ec0401b7e4
sergapit1 <serg.apit0@gmail.com>
2020-04-25 01:49:53 +0300

    Ready1

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, input: ");
+
+        for (int i = 0; i < 3; i++) {
+            try {
+                int b = scanner.nextInt();
+                if (b > 0 && b <= 10) {
+                    System.out.println("Input correct: " + b);
+
+                } else {
+                    System.err.println("Error: " + b);
+                    i--;
+                    throw new NumberFormatException();
+                }
+
+            } catch (Exception e) {
+                System.err.println("Need to enter a number 0 - 10 : " + e);
+
+            }
+        }
+    }
+}
 
